9fbc3e4d5ee49ea76b0fdb08f02051ecefdf1054
Gerald Nelson <nergy_nelson@proton.me>
2026-01-31 20:02:33 +0900

    chore: Remove outdated router implementation
    - These simply do not apply anymore, as the fundamental
      design is flawed. Instead, the router will be replaced
      with proper 8x8 AXI-S crossbar later.
    
    Signed-off-by: Gerald Nelson <nergy_nelson@proton.me>

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,20 +2,5 @@ package nero
 
 import circt.stage.ChiselStage
 import nero.router._
-import nero.neuron.Neuron
 
-object Main extends App {
-  println("Neuron")
-  println("CHIRRTL: ")
-  println(ChiselStage.emitCHIRRTL(new Neuron(0, 0, true, 4, 4)))
-
-  println("SystemVerilog: ")
-  println(ChiselStage.emitSystemVerilog(new Neuron(0, 0, true, 4, 4)))
-
-  println("Inbound Port")
-  println("CHIRRTL: ")
-  println(ChiselStage.emitCHIRRTL(new InboundPort(0, 0, 4, 4)))
-
-  println("SystemVerilog: ")
-  println(ChiselStage.emitSystemVerilog(new InboundPort(0, 0, 4, 4)))
-}
+object Main extends App {}
